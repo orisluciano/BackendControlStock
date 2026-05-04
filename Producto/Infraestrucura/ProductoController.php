a37088a75510b3c8cc3145232501261348fbd914
Luciano <orisluciano@gmail.com>
2026-05-04 12:19:08 -0300

    producto - estructura de carpetas y apicontroller

---
+++ Producto/Infraestrucura/ProductoController.php
@@ -0,0 +1,11 @@
+<?php
+class ProductoController implements IApiController
+{
+    public function _ejecutar(){}
+    public function _get(){}
+    public function _post(){}
+    public function _put(){}
+    public function _delete(){}
+}
+
+?>
